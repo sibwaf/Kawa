306c4ff4edcddcb502ff08f52ba23c3e000ea1f6
sibwaf <7131069+sibwaf@users.noreply.github.com>
2020-11-23 19:06:45 +0300

    Rule for checking implicit always-null returns

---
+++ Launcher/src/main/kotlin/sibwaf/kawa/rules/R0003_ImplicitNullReturn.kt
@@ -0,0 +1,20 @@
+package sibwaf.kawa.rules
+
+import sibwaf.kawa.constraints.Nullability
+import sibwaf.kawa.constraints.ReferenceConstraint
+import spoon.reflect.code.CtLiteral
+import spoon.reflect.code.CtReturn
+
+class R0003_ImplicitNullReturn : Rule() {
+
+    override fun <R : Any?> visitCtReturn(returnStatement: CtReturn<R>) {
+        val flow = getFlow(returnStatement) ?: return
+        val frame = getFrame(flow, returnStatement) ?: return
+        val expression = returnStatement.returnedExpression.takeUnless { it is CtLiteral<*> } ?: return
+
+        val (_, constraint) = getValue(frame, expression)
+        if (constraint is ReferenceConstraint && constraint.nullability == Nullability.ALWAYS_NULL) {
+            warn("The returned value is always null: '${toSimpleString(expression)}'", expression);
+        }
+    }
+}
--- Launcher/src/main/kotlin/sibwaf/kawa/rules/Rule.kt
+++ Launcher/src/main/kotlin/sibwaf/kawa/rules/Rule.kt
@@ -1,12 +1,20 @@
 package sibwaf.kawa.rules
 
+import kotlinx.coroutines.runBlocking
 import sibwaf.kawa.DataFrame
+import sibwaf.kawa.EmptyFlow
 import sibwaf.kawa.MethodFlow
+import sibwaf.kawa.ValueCalculator
 import sibwaf.kawa.Warning
+import sibwaf.kawa.values.ConstrainedValue
 import spoon.reflect.code.CtBlock
+import spoon.reflect.code.CtExpression
+import spoon.reflect.code.CtInvocation
 import spoon.reflect.code.CtStatement
+import spoon.reflect.code.CtVariableAccess
 import spoon.reflect.declaration.CtElement
 import spoon.reflect.declaration.CtExecutable
+import spoon.reflect.declaration.CtVariable
 import spoon.reflect.visitor.CtAbstractVisitor
 
 abstract class Rule : CtAbstractVisitor() {
@@ -38,12 +46,27 @@ abstract class Rule : CtAbstractVisitor() {
         return null
     }
 
+    fun getValue(frame: DataFrame, expression: CtExpression<*>): ConstrainedValue {
+        return runBlocking {
+            ValueCalculator.calculateValue(MethodFlow(), frame, expression, { flow[it] ?: EmptyFlow })
+        }
+    }
+
     fun getStatement(element: CtElement): CtStatement? {
         var statement: CtElement = element
         while (statement.parent !is CtBlock<*>) {
             statement = statement.parent ?: return null
         }
         return statement as CtStatement
+    }
+
+    fun toSimpleString(element: CtElement): String {
+        return when (element) {
+            is CtVariable<*> -> element.simpleName
+            is CtVariableAccess<*> -> element.variable.simpleName
+            is CtInvocation<*> -> "${element.executable.simpleName}()"
+            else -> element.toString()
+        }
     }
 
     fun warn(message: String, element: CtElement) {
