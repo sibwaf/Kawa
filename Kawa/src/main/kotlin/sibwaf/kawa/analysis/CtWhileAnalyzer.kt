5493e8d51a2c91be67fe099cbc34bce09ad71f5d
sibwaf <7131069+sibwaf@users.noreply.github.com>
2020-11-29 17:15:55 +0300

    Rework CtWhileAnalyzer

--- Kawa/src/main/kotlin/sibwaf/kawa/analysis/CtWhileAnalyzer.kt
+++ Kawa/src/main/kotlin/sibwaf/kawa/analysis/CtWhileAnalyzer.kt
@@ -1,9 +1,11 @@
 package sibwaf.kawa.analysis
 
 import sibwaf.kawa.DataFrame
+import sibwaf.kawa.MutableDataFrame
 import spoon.reflect.code.CtBreak
 import spoon.reflect.code.CtStatement
 import spoon.reflect.code.CtWhile
+import java.util.LinkedList
 
 class CtWhileAnalyzer : StatementAnalyzer {
 
@@ -12,46 +14,28 @@ class CtWhileAnalyzer : StatementAnalyzer {
     override suspend fun analyze(state: StatementAnalyzerState, statement: CtStatement): DataFrame {
         statement as CtWhile
 
-        // TODO: if condition is always true even on the second run, it could be an infinite loop
         // FIXME: leaving frames/values behind here is probably very-very bad, recalc after second run?
 
         val localState = state.copy(jumpPoints = ArrayList())
 
-        // var lastGuaranteedFrame: DataFrame = MutableDataFrame(state.frame)
-        // var lastGuaranteedFrameFound = false
-        lateinit var lastExitFrame: DataFrame
-        var currentState = localState
+        val startFrames = LinkedList<DataFrame>().also { it += MutableDataFrame(localState.frame) }
+        val exitFrames = LinkedList<DataFrame>()
 
         for (iteration in 0 until 2) {
-            val (thenFrame, elseFrame, _, conditionConstraint) = currentState.getConditionValue(statement.loopingExpression)
-            lastExitFrame = elseFrame
+            val entryFrame = DataFrame.merge(localState.frame, startFrames)
+            val iterationState = localState.copy(frame = entryFrame)
 
-            if (conditionConstraint.isFalse) {
-                break
-            }
+            val (thenFrame, elseFrame, _, _) = iterationState.getConditionValue(statement.loopingExpression)
 
-            val bodyFrame = currentState.copy(frame = thenFrame).getStatementFlow(statement.body)
+            val bodyFrame = localState.copy(frame = thenFrame).getStatementFlow(statement.body)
+            startFrames += bodyFrame.compact(localState.frame)
 
-            if (conditionConstraint.isTrue) {
-                /*if (!lastGuaranteedFrameFound) {
-                    lastGuaranteedFrame = thenFrame
-                }*/
-
-                currentState = currentState.copy(frame = bodyFrame)
-            } else {
-                // lastGuaranteedFrameFound = true
-
-                val nextFrame = DataFrame.merge(
-                        currentState.frame,
-                        bodyFrame.compact(currentState.frame),
-                        elseFrame.compact(currentState.frame)
-                )
-
-                currentState = currentState.copy(frame = nextFrame)
-            }
+            // If the condition is always true, this frame will be unreachable
+            // and won't affect anything
+            exitFrames += elseFrame.compact(localState.frame)
         }
 
-        var resultFrame = lastExitFrame.compact(state.frame)
+        var resultFrame = DataFrame.merge(state.frame, exitFrames)
 
         for (jump in localState.jumpPoints) {
             // TODO: check labels
@@ -63,11 +47,5 @@ class CtWhileAnalyzer : StatementAnalyzer {
         }
 
         return resultFrame
-
-        /*DataFrame.merge(
-                state.frame,
-                lastGuaranteedFrame.compact(state.frame),
-                lastExitFrame.compact(state.frame)
-        )*/
     }
 }
