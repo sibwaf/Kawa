fe8a68ebc5b809a7037fa40770a0550e465c61b3
sibwaf <7131069+sibwaf@users.noreply.github.com>
2020-11-30 22:46:32 +0300

    Handle for-each variable

--- Kawa/src/main/kotlin/sibwaf/kawa/analysis/CtForEachAnalyzer.kt
+++ Kawa/src/main/kotlin/sibwaf/kawa/analysis/CtForEachAnalyzer.kt
@@ -1,8 +1,13 @@
 package sibwaf.kawa.analysis
 
+import sibwaf.kawa.DataFrame
+import sibwaf.kawa.MutableDataFrame
+import sibwaf.kawa.UnreachableFrame
 import sibwaf.kawa.calculation.conditions.ConditionCalculatorResult
 import sibwaf.kawa.constraints.BooleanConstraint
+import sibwaf.kawa.constraints.ReferenceConstraint
 import sibwaf.kawa.values.BooleanValue
+import sibwaf.kawa.values.Value
 import sibwaf.kawa.values.ValueSource
 import spoon.reflect.code.CtForEach
 import spoon.reflect.code.CtStatement
@@ -21,4 +26,32 @@ class CtForEachAnalyzer : CtLoopAnalyzer<CtForEach>() {
 
     override suspend fun getPostCondition(state: StatementAnalyzerState, loop: CtForEach): ConditionCalculatorResult? =
             null
+
+    override suspend fun getBodyFlow(state: StatementAnalyzerState, loop: CtForEach): DataFrame {
+        val frame = MutableDataFrame(state.frame).apply {
+            val variable = loop.variable
+            val value = Value.from(loop.variable, ValueSource.NONE)
+
+            setValue(variable, value)
+            setConstraint(value, ReferenceConstraint.createUnknown())
+        }
+
+        return super.getBodyFlow(state.copy(frame = frame), loop)
+    }
+
+    override suspend fun analyze(state: StatementAnalyzerState, statement: CtStatement): DataFrame {
+        statement as CtForEach
+
+        val (frame, _) = state.getValue(statement.expression)
+
+        val resultFrame = super.analyze(state.copy(frame = frame), statement).compact(state.frame)
+        return if (resultFrame is UnreachableFrame) {
+            val cleanedFrame = resultFrame.previous
+                    .copy(retiredVariables = listOf(statement.variable))
+
+            UnreachableFrame.after(cleanedFrame)
+        } else {
+            resultFrame.copy(retiredVariables = listOf(statement.variable))
+        }
+    }
 }
