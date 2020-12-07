233efa0e615321d6f1c86b751302d4895d38a2a2
sibwaf <7131069+sibwaf@users.noreply.github.com>
2020-12-07 23:05:53 +0300

    Delegate CtAssignmentAnalyzer to CtAssignmentCalculator

--- Kawa/src/main/kotlin/sibwaf/kawa/analysis/CtAssignmentAnalyzer.kt
+++ Kawa/src/main/kotlin/sibwaf/kawa/analysis/CtAssignmentAnalyzer.kt
@@ -1,38 +1,7 @@
 package sibwaf.kawa.analysis
 
-import sibwaf.kawa.AnalyzerState
-import sibwaf.kawa.DataFrame
-import sibwaf.kawa.MethodPurity
-import sibwaf.kawa.MutableDataFrame
-import spoon.reflect.code.CtAssignment
-import spoon.reflect.code.CtFieldWrite
-import spoon.reflect.code.CtStatement
-import spoon.reflect.code.CtVariableAccess
+import sibwaf.kawa.calculation.CtAssignmentCalculator
 
-class CtAssignmentAnalyzer : StatementAnalyzer {
-
-    override fun supports(statement: CtStatement) = statement is CtAssignment<*, *>
-
-    override suspend fun analyze(state: AnalyzerState, statement: CtStatement): DataFrame {
-        statement as CtAssignment<*, *>
-
-        val target = statement.assigned
-        if (target is CtFieldWrite<*>) {
-            state.annotation.purity = MethodPurity.DIRTIES_THIS // TODO
-        } else {
-            val targetVariable = (target as? CtVariableAccess<*>)
-                ?.variable
-                ?.declaration
-
-            if (targetVariable != null) {
-                val (frame, result) = state.getValue(statement.assignment)
-                return MutableDataFrame(frame).apply {
-                    setValue(targetVariable, result.value)
-                    setConstraint(result.value, result.constraint)
-                }
-            }
-        }
-
-        return state.frame
-    }
+class CtAssignmentAnalyzer : ExpressionStatementAnalyzer() {
+    override val calculator = CtAssignmentCalculator()
 }
